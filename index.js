c5de7c7a6990fcbaeb7d60b100c0a95ee9d7160c
tegaadigu <tega.adigu@gmail.com>
2015-10-29 17:48:18 +0800

    Changed index.js to accept / broadcast messages to particular chat rooms

--- index.js
+++ index.js
@@ -6,20 +6,43 @@ app.get('/', function(req, res){
   // res.send('<h1>Hello World</h1>'); //Function to send out message to the client...
   // Since we'll be sending out an html its best to use sendFile();
   res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/message', function(req, res) {
+  // console.log(req.query);
+  // console.log(res.query);
+  if(req.query.room)
+    console.log('yes room is: '+req.query.room);
+
+    if(req.query.msg)
+      console.log('msg is :'+req.query.msg);
+
+
+  io.sockets.to(req.query.room).emit('chat-message', req.query.msg);
 })
 
 // Bind connection on web socket.
 io.on('connection', function(socket){
-    console.log('A user connected');
+    // Join Private channel..
+    socket.on('join', function(room){
+      console.log('A user connected to room-' + room);
+      socket.join(room);
+      socket.on('chat-message', function(msg){
+        console.log("message to room-"+room+ ": "+msg);
+        // io.emit('chat-message', msg);
+        socket.broadcast.to(room).emit('chat-message', msg);
+      })
+    })
 
-    socket.on('disconnect', function(){ //bind disconnect event to socket.
-      console.log('A user disconnected');
+    socket.on('disconnect', function(room){ //bind disconnect event to socket.
+      console.log('A user disconnected from room: '+room);
     });
 
-    socket.on('chat-message', function(msg){
-      console.log("message: "+msg);
-      io.emit('chat-message', msg);
-    })
+    // socket.on('chat-message', function(msg){
+    //   console.log("message: "+msg);
+    //   // io.emit('chat-message', msg);
+    //   // socket.broadcast.emit('chat-message', msg);
+    // })
 })
 
 
